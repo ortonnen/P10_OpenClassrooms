b2931268fc7c43db207a71b1ac64db1345986d9b
ortonnen <ortonne.n@gmail.com>
2020-09-28 13:52:50 +0200

    custom Recipes cell

---
+++ Reciplease/View/RecipesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipesTableViewCell.swift
+//  Reciplease
+//
+//  Created by Nathalie Ortonne on 28/09/2020.
+//  Copyright © 2020 Nathalie Ortonne. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
