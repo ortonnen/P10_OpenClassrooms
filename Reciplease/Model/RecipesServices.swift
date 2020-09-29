7acfa6f0498d5a1c7c9d1bd178a5e80168fb29ec
ortonnen <ortonne.n@gmail.com>
2020-09-29 11:01:10 +0200

    create request with alamo fire

--- Reciplease/Model/RecipesServices.swift
+++ Reciplease/Model/RecipesServices.swift
@@ -7,53 +7,30 @@
 //
 
 import Foundation
+import Alamofire
 
 class RecipesServices {
 
     //MARK: Properties
-    
     private let recipesURL = URL(string: "https://api.edamam.com/search?")!
+    private let apikey = "1ca6fa53868439f0dff48a62bed5b933"
+    private let apiId = "ae5de728"
 
-    private var recipesSession = URLSession(configuration: .default)
-    private var task: URLSessionDataTask?
+    func getRecipes(with ingredient: String, callback: @escaping (Bool, Recipes?) -> Void) {
 
-    init(recipesSession: URLSession){
-        self.recipesSession = recipesSession
-    }
+        guard let url = URL(string: "\(recipesURL)&q=\(ingredient)))&app_id=\(apiId)&app_key=\(apikey)") else { return }
 
-    private func createRecipesRequest(with
-        ingredients: String) -> URLComponents {
-        var component = URLComponents(url: recipesURL, resolvingAgainstBaseURL: true)
-
-        component?.queryItems = [URLQueryItem(name: "q", value: "\(ingredients)"),
-                                 URLQueryItem(name: "app_key", value: "1ca6fa53868439f0dff48a62bed5b933"),
-                                 URLQueryItem(name: "app_id", value: "ae5de728")]
-        return component!
-    }
-
-    func getRecipes(with ingredients: String, callback: @escaping (Bool, Recipes?) -> Void) {
-        let request = createRecipesRequest(with: ingredients)
-        guard let urlRequest = request.url else { return }
-
-        task?.cancel()
-        task = recipesSession.dataTask(with: urlRequest) { (data, response, error) in
-            DispatchQueue.main.async {
-
-                guard let data = data, error == nil else {
+        Alamofire.request(url).responseJSON { (response) in
+            if response.data != nil {
+                do {
+                    let responseJSON = try JSONDecoder().decode(Recipes.self, from: response.data!)
+                    callback(true, responseJSON)
+                } catch {
                     callback(false, nil)
-                    return
                 }
-                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                    callback(false, nil)
-                    return
-                }
-                guard let recipes = try? JSONDecoder().decode(Recipes.self, from: data) else {
-                    callback(false, nil)
-                    return
-                }
-                callback(true, recipes)
             }
         }
-        task?.resume()
     }
 }
+
+
