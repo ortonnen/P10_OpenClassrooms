c3c847346f4678c64f45550e594b2cd22bf62eac
ortonnen <ortonne.n@gmail.com>
2020-10-13 15:25:48 +0200

    minor correction

--- Reciplease/Controller/FavoriteRecipesViewController.swift
+++ Reciplease/Controller/FavoriteRecipesViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class FavoriteRecipesViewController: UIViewController {
 
-    var favoriteRecipes = FavoriteRecipe()
+var favoriteRecipe = FavoriteRecipe()
 
     @IBOutlet weak var FavoriteRecipeTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(favoriteRecipe.recipes)
         // Do any additional setup after loading the view.
     }
     
@@ -30,7 +30,7 @@ extension FavoriteRecipesViewController: UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return favoriteRecipe.recipes.count
 
     }
 
@@ -39,7 +39,7 @@ extension FavoriteRecipesViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteRecipeCell", for: indexPath) as? FavoriteRecipesTableViewCell else {
             return UITableViewCell()
         }
-       
+        cell.configure(withTitle: favoriteRecipe.recipes[indexPath.row].label, subTitle: favoriteRecipe.recipes[indexPath.row].ingredients[indexPath.row].text, imageUrl: favoriteRecipe.recipes[indexPath.row].image)
         return cell
     }
 }
