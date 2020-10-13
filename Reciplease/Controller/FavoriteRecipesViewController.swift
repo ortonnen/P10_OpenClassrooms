e74a11bf3c3aa6cb7c14bb41bb4cc84a8997806f
ortonnen <ortonne.n@gmail.com>
2020-10-13 11:02:41 +0200

    add extension for TableView For favoriteRecipes

--- Reciplease/Controller/FavoriteRecipesViewController.swift
+++ Reciplease/Controller/FavoriteRecipesViewController.swift
@@ -12,6 +12,8 @@ class FavoriteRecipesViewController: UIViewController {
 
     var favoriteRecipes = FavoriteRecipe()
 
+    @IBOutlet weak var FavoriteRecipeTableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,14 +21,26 @@ class FavoriteRecipesViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+//MARK: TableView
+extension FavoriteRecipesViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteRecipeCell", for: indexPath) as? FavoriteRecipesTableViewCell else {
+            return UITableViewCell()
+        }
+       
+        return cell
+    }
+}
+
