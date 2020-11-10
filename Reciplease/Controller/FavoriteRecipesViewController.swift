40a2517b05edc8eccb3ddd8a9c1ed5ccb7e8dc7c
ortonnen <ortonne.n@gmail.com>
2020-11-10 13:28:15 +0100

    add func to delete cell in tableView and minor correction

--- Reciplease/Controller/FavoriteRecipesViewController.swift
+++ Reciplease/Controller/FavoriteRecipesViewController.swift
@@ -9,29 +9,28 @@
 import UIKit
 
 class FavoriteRecipesViewController: UIViewController {
+    var favoriteRecipes = FavoriteRecipes.all
 
-var favoriteRecipe = FavoriteRecipe()
+    @IBOutlet weak var favoriteRecipeTableView: UITableView!
 
-    @IBOutlet weak var FavoriteRecipeTableView: UITableView!
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       
-        // Do any additional setup after loading the view.
+        favoriteRecipes = FavoriteRecipes.all
+        favoriteRecipeTableView.reloadData()
+        print(favoriteRecipes.count)
+
     }
-    
-
-
 }
 //MARK: TableView
-extension FavoriteRecipesViewController: UITableViewDataSource {
+extension FavoriteRecipesViewController: UITableViewDataSource, UITableViewDelegate {
 
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return favoriteRecipe.recipes.count
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoriteRecipes.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -39,8 +38,22 @@ extension FavoriteRecipesViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteRecipeCell", for: indexPath) as? FavoriteRecipesTableViewCell else {
             return UITableViewCell()
         }
-        cell.configure(withTitle: favoriteRecipe.recipes[indexPath.row].label, subTitle: favoriteRecipe.recipes[indexPath.row].ingredients[indexPath.row].text, imageUrl: favoriteRecipe.recipes[indexPath.row].image)
+        guard favoriteRecipes.count > 0 else {
+            return cell
+        }
+        
+        cell.configure(withTitle: favoriteRecipes[indexPath.row].name ?? "No Recipe", subTitle: favoriteRecipes[indexPath.row].ingredients ?? "", imageUrl: favoriteRecipes[indexPath.row].urlImage ?? "https://www.edamam.com/web-img/d32/d32b4dc2e7bd9d4d1a24bbced0c89143.jpg")
+
         return cell
     }
-}
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        guard let recipe = favoriteRecipes[indexPath.row].name else { return }
+
+        CoreDataManager.deleteRecipe(recipe)
+        favoriteRecipes.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 
