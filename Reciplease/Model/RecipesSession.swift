3bc35d48016feb49a2eb8cae774e696d6a93fa7b
ortonnen <ortonne.n@gmail.com>
2020-11-10 13:34:24 +0100

    add protocol AlamoSession

---
+++ Reciplease/Model/RecipesSession.swift
@@ -0,0 +1,29 @@
+//
+//  RecipesSession.swift
+//  Reciplease
+//
+//  Created by Nathalie Ortonne on 16/10/2020.
+//  Copyright © 2020 Nathalie Ortonne. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+// MARK: Protocol AlamoSession
+protocol AlamoSession {
+    func request(with url: URL, callBack: @escaping (AFDataResponse<Any>) -> Void)
+}
+
+
+// MARK: RecipeSession
+
+class RecipeSession: AlamoSession {
+    func request(with url: URL, callBack: @escaping (AFDataResponse<Any>) -> Void) {
+        AF.request(url)
+            .validate(statusCode: 200 ..< 400)
+            .responseJSON { (data) in
+            callBack(data)
+        }
+    }
+}
+
