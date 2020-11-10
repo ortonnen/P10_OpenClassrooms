cdc57a9a3a2f56982a8151b9caef9e1c9df8b416
ortonnen <ortonne.n@gmail.com>
2020-11-10 13:25:04 +0100

    update Alamofire

--- Podfile
+++ Podfile
@@ -2,7 +2,7 @@ platform :ios, '11.0'
 
 use_frameworks!
 
-pod 'Alamofire', '5.0.1'
+pod 'Alamofire', '~> 5.2'
 pod 'AlamofireImage', '4.0.0'
 
 target "Reciplease" do
---
+++ RecipleaseTests/AlamofireSessionFake.swift
@@ -0,0 +1,42 @@
+//
+//  URLSessionFake.swift
+//  RecipleaseTests
+//
+//  Created by Nathalie Ortonne on 18/09/2020.
+//  Copyright © 2020 Nathalie Ortonne. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+@testable import Reciplease
+
+struct FakeResponse {
+    var response: HTTPURLResponse?
+    var data: Data?
+}
+
+
+class AlamofireSessionFake: AlamoSession {
+
+    // MARK: - Properties
+
+    private let fakeResponse: FakeResponse
+
+    // MARK: - Initializer
+
+    init(fakeResponse: FakeResponse) {
+        self.fakeResponse = fakeResponse
+    }
+
+    //MARK: Methode
+
+    func request(with url: URL, callBack : @escaping (AFDataResponse<Any>) -> Void) {
+        guard let httpResponse = fakeResponse.response else {return}
+        guard let requestData = fakeResponse.data else {return}
+        let urlRequest = URLRequest(url: URL(string: "https://www.apple.com")!)
+        let result = try? JSONSerialization.data(withJSONObject: requestData, options: .fragmentsAllowed)
+
+        callBack(AFDataResponse<Any>(request: urlRequest, response: httpResponse, data: requestData, metrics: .none, serializationDuration: .zero, result:result as! Result<Any, AFError>))
+    }
+
+}
